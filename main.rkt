f9cdf4232549dd45e74597ad5fe30ca25dca39ee
Jesse Alama <jesse@lisp.sh>
2017-12-26 06:18:35 +0100

    Provide it.

--- main.rkt
+++ main.rkt
@@ -1,12 +1,16 @@
 #lang racket/base
 
 (provide json-pointer?
+         json-pointer-expression?
+         expression->pointer
+         parse-json-pointer
          json-pointer-value)
 
 (require (only-in (file "expr.rkt")
                   json-pointer-expression?)
          (only-in (file "parser.rkt")
                   json-pointer?
+                  parse-json-pointer
                   expression->pointer)
          (only-in (file "eval.rkt")
                   json-pointer-value))
